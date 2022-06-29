9b881b2d0278e9d92bba51d69a23e31ff670ede1
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-06-29 10:56:59 -0300

    Projeto no inicio de desenvolvimento

--- superninjas/src/App.js
+++ superninjas/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import logo from './assets/images/superninjas.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GlobalStyled = createGlobalStyle `
+  body {
+    margin: 0;
+  }
+`
+const Header = styled.header`
+background-color: #BB94D4;
+padding: 20px;
+display: flex;
+justify-content: space-between;
+ img {
+  height: 100px;
+  padding-left: 30px;
+ }
+ button {
+  margin-top: 40px;
+  margin-left: 5px;
+ }
+`
+const Buttons = styled.div`
+margin-right: 50px;
+`
+const BodyHome = styled.div`
+  text-align: center;
+  margin-top: 200px;
+`
 
-export default App;
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <GlobalStyled/>
+        <Header>
+          <img src={logo} alt='Logo do Superninjas'/>
+          <Buttons>
+            <button>home</button>
+            <button>carrinho</button>
+          </Buttons>
+        </Header>
+        <BodyHome>
+          <h1>SuperNinjas</h1>
+          <h3>O talento certo no momento certo!</h3>
+          <button>Quero ser um SuperNinja</button>
+          <button>Contratar um SuperNinja</button>
+        </BodyHome>
+      </div>
+    );
+  }
+};
